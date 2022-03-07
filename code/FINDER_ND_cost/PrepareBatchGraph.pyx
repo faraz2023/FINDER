8a208f77ef81f33251ebf58c18e5075d177cb5f0
Faraz Khoshbakhtian <farazkhoshbakhtian@Farazs-MacBook-Pro.local>
2022-03-07 14:33:52 -0500

    fixed output type for sparsetensors

--- code/FINDER_ND_cost/PrepareBatchGraph.pyx
+++ code/FINDER_ND_cost/PrepareBatchGraph.pyx
@@ -133,8 +133,7 @@ cdef class py_PrepareBatchGraph:
         rowNum= matrix.rowNum
         colNum= matrix.colNum
         indices = np.mat([rowIndex, colIndex]).transpose()
-        return torch.sparse_coo_tensor(np.transpose(np.array(indices)), np.array(data), size=torch.Size([rowNum,colNum])).coalesce()\
-            .type(torch.FloatTensor)
+        return torch.sparse_coo_tensor(np.transpose(np.array(indices)), np.array(data), size=torch.Size([rowNum,colNum]), dtype=torch.float).coalesce()
         #return tf.SparseTensorValue(indices, data, (rowNum,colNum))
 
 
