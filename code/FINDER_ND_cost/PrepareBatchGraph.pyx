3da40b78cf067a7049a0c5c09cb5ce2606438dbc
Faraz Khoshbakhtian <farazkhoshbakhtian@Farazs-MacBook-Pro.local>
2022-03-07 17:25:00 -0500

    added coalesce

--- code/FINDER_ND_cost/PrepareBatchGraph.pyx
+++ code/FINDER_ND_cost/PrepareBatchGraph.pyx
@@ -137,7 +137,7 @@ cdef class py_PrepareBatchGraph:
         indices = np.mat([rowIndex, colIndex]).transpose()
 
         index = torch.tensor(np.transpose(np.array(indices)))
-        value = torch.Tensor(np.array(data))
+        value = torch.tensor(np.array(data))
         index, value = torch_sparse.coalesce(index, value, m=rowNum, n=colNum)
         return_dict = {"index": index, "value": value, "m":rowNum, "n":colNum}
 
