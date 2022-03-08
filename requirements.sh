30e12fc8a72223518d1fd7e6c4f364b525d3cc97
faraz.2023 <faraz.khoshbakhtian@mail.utoronto.ca>
2022-03-07 19:02:10 -0500

    requirments scripts

---
+++ requirements.sh
@@ -0,0 +1,11 @@
+conda install -c pytorch pytorch=1.10.2
+conda install -c conda-forge tqdm=4.63.0
+conda install -c conda-forge networkx=2.5
+conda install -c anaconda scipy=1.7.3
+conda install -c anaconda networkx=2.5
+conda install -c anaconda cython=0.29.5
+conda install pyg=2.0.3 -c pyg -c conda-forge
+conda install -c anaconda pandas=1.4.1
+
+
+
