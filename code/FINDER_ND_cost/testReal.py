f19c551ad693a2c4756a8d400f823b219792d55b
faraz2023 <faraz.khoshbakhtian@mail.utoronto.ca>
2022-03-10 17:52:44 -0500

    w0.01 for all nodes added

--- code/FINDER_ND_cost/testReal.py
+++ code/FINDER_ND_cost/testReal.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
+
 import sys,os
 sys.path.append(os.path.dirname(__file__) + os.sep + '../')
 from FINDER_torch import FINDER
@@ -26,7 +25,7 @@ def GetSolution(STEPRATIO, MODEL_FILE):
     #data_test_name = ['Crime', 'HI-II-14']
     #data_test_costType = ['degree', 'random']
     data_test_name = ['HI-II-14', 'Digg']
-    data_test_costType = ['zero', 'degree']
+    data_test_costType = ['001', 'zero', 'degree']
     #model_file = './FINDER_ND_cost/models/%s'%MODEL_FILE
     model_file = './models/TORCH-Model_barabasi_albert/{}'.format(MODEL_FILE)
     ## save_dir
@@ -36,11 +35,13 @@ def GetSolution(STEPRATIO, MODEL_FILE):
     
     save_dir_degree = save_dir + '/Data_degree'
     save_dir_zero = save_dir + '/Data_zero'
+    save_dir_001 = save_dir + '/Data_001'
+
 
     #save_dir_random = save_dir + '/Data_random'
     mkdir(save_dir_degree)
     mkdir(save_dir_zero)
-    #os.mkdir(save_dir_random)
+    mkdir(save_dir_001)
 
     ## begin computing...
     print('The best model is :%s' % (model_file))
@@ -60,6 +61,8 @@ def GetSolution(STEPRATIO, MODEL_FILE):
                 solution, time = dqn.EvaluateRealData(model_file, data_test, save_dir_random, stepRatio)
             elif costType == 'zero':
                 solution, time = dqn.EvaluateRealData(model_file, data_test, save_dir_zero, stepRatio)
+            elif costType == '001':
+                solution, time = dqn.EvaluateRealData(model_file, data_test, save_dir_001, stepRatio)
 
             df.iloc[0, j] = time
 
@@ -69,6 +72,8 @@ def GetSolution(STEPRATIO, MODEL_FILE):
             save_dir_local = save_dir_random + '/StepRatio_%.4f' % stepRatio
         elif costType == 'zero':
             save_dir_local = save_dir_zero + '/StepRatio_%.4f' % stepRatio
+        elif costType == '001':
+            save_dir_local = save_dir_001 + '/StepRatio_%.4f' % stepRatio
             
         if not os.path.exists(save_dir_local):
             mkdir(save_dir_local)
@@ -87,11 +92,12 @@ def EvaluateSolution(STEPRATIO, STRTEGYID):
     #data_test_name = ['Crime', 'HI-II-14']
     #data_test_costType = ['degree', 'random']
     data_test_name = ['HI-II-14', 'Digg']
-    data_test_costType = ['zero', 'degree']
+    data_test_costType = ['001', 'zero', 'degree']
 
     ## save_dir
     save_dir_degree = '../results/FINDER_ND_cost/real/Data_degree/StepRatio_%.4f/' % STEPRATIO
-    save_dir_zero = '../results/old_FINDER_ND_cost_tf/real/Data_zero/StepRatio_%.4f/' % STEPRATIO
+    save_dir_zero = '../results/FINDER_ND_cost/real/Data_zero/StepRatio_%.4f/' % STEPRATIO
+    save_dir_001 = '../results/FINDER_ND_cost/real/Data_001/StepRatio_%.4f/' % STEPRATIO
 
     #save_dir_random = '../results/FINDER_ND_cost/real/Data_random/StepRatio_%.4f/' % STEPRATIO
     ## begin computing...
@@ -108,6 +114,8 @@ def EvaluateSolution(STEPRATIO, STRTEGYID):
                 solution = save_dir_random + data_test_name[i] + '_random.txt'
             elif costType == 'zero':
                 solution = save_dir_zero + data_test_name[i] + '_zero.txt'
+            elif costType == '001':
+                solution = save_dir_001 + data_test_name[i] + '_001.txt'
 
             t1 = time.time()
             # strategyID: 0:no insert; 1:count; 2:rank; 3:multiply
@@ -123,6 +131,8 @@ def EvaluateSolution(STEPRATIO, STRTEGYID):
                 result_file = save_dir_random + '/MaxCCList_Strategy_' + data_test_name[i] + '.txt'
             elif costType == 'zero':
                 result_file = save_dir_zero + '/MaxCCList_Strategy_' + data_test_name[i] + '.txt'
+            elif costType == '001':
+                result_file = save_dir_001 + '/MaxCCList_Strategy_' + data_test_name[i] + '.txt'
 
 
             with open(result_file, 'w') as f_out:
@@ -137,6 +147,8 @@ def EvaluateSolution(STEPRATIO, STRTEGYID):
             df.to_csv(save_dir_random + '/solution_%s_score.csv' % (costType), encoding='utf-8', index=False)
         elif costType == 'zero':
             df.to_csv(save_dir_zero + '/solution_%s_score.csv' % (costType), encoding='utf-8', index=False)
+        elif costType == '001':
+            df.to_csv(save_dir_001 + '/solution_%s_score.csv' % (costType), encoding='utf-8', index=False)
 
 def main():
     model_file = 'nrange_30_50_iter_944400.ckpt'
